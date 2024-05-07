f630da01be1df375bd3918c5447770104e1499b9
Josue Victor <victorarroxellas01@gmail.com>
2024-05-07 20:21:04 -0300

    concluído o exercício 55 + constantes em objetos

--- 10_constantes_em_objetos/index.php
+++ 10_constantes_em_objetos/index.php
@@ -1,1 +1,18 @@
 <?php
+
+class Humano{
+    const OLHOS = 2;
+    const BRACOS = 2;
+    const PERNAS = 2;
+
+    public function mostrarConstatnte()
+    {
+        echo self::BRACOS;
+    }
+}
+
+$victor = new Humano;
+
+echo $victor::OLHOS . PHP_EOL;
+
+$victor->mostrarConstatnte();
--- 9_exercicio_55/index.php
+++ 9_exercicio_55/index.php
@@ -1,1 +1,25 @@
 <?php
+
+class Carro{
+    public  $cor = 'branco';
+    public $modelo = '320i';
+    public $velocidadeMaxima = 240;
+
+    public function setVelocidade($velocidade)
+    {
+        $this->velocidadeMaxima = $velocidade;
+    }
+
+    public function getVelocidadeMaxima()
+    {
+        return $this->velocidadeMaxima;
+    }
+}
+
+$bmw = new Carro;
+
+echo $bmw->velocidadeMaxima  . PHP_EOL;
+
+$bmw->setVelocidade(180);
+
+echo "Reduziu para:" . $bmw->getVelocidadeMaxima();
