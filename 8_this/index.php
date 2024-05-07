9d34f269877d3590010c07ff10b761843295bd05
Josué Victor <josue.arroxellas@unimedmaceio.com.br>
2024-05-07 15:27:06 -0300

    finalizando estudos do this

--- 8_this/index.php
+++ 8_this/index.php
@@ -7,4 +7,26 @@ class Animal{
     {
         $this->nome = $nome;
     }
+
+    function latir()
+    {
+        return 'Au Au';
+    }
+
+    function latirForte()
+    {
+        return strtoupper($this->latir());
+    }
 }
+
+$cachorro = new Animal;
+
+echo "o nome do animal é: $cachorro->nome <br>";
+
+$cachorro->escolherNome('toto');
+
+echo "o nome do animal é: $cachorro->nome <br>";
+
+echo $cachorro->latir();
+echo $cachorro->latirForte();
+
