4bf6fecd0e8de8417dc60a6c3e66dae2baf0b9a9
Josué Victor <josue.arroxellas@unimedmaceio.com.br>
2024-05-08 17:13:36 -0300

    fazendo o exercicio gerenciador de tarefas

--- 21_gerenciador_de_tarefas/index.php
+++ 21_gerenciador_de_tarefas/index.php
@@ -1,1 +1,39 @@
 <?php
+
+    class Task{
+
+        public $title;
+        public $description;
+        public $completed;
+
+        public function __constructor($title, $description, $completed)
+        {
+
+        }
+
+        public function markAsCompleted()
+        {
+            
+        }
+
+        public function markAsIncomplete()
+        {
+            
+        }
+
+        public function getTitle()
+        {
+            
+        }
+
+        public function getDescription()
+        {
+            
+        }
+
+        public function isCompleted()
+        {
+            
+        }
+
+    }
