7dc08df63f87fabbb54620f646afa79f477d6fc8
Josue Victor <victorarroxellas01@gmail.com>
2024-05-09 07:40:50 -0300

    gerenciador de tarefas

--- 21_gerenciador_de_tarefas/index.php
+++ 21_gerenciador_de_tarefas/index.php
@@ -6,34 +6,55 @@
         public $description;
         public $completed;
 
-        public function __constructor($title, $description, $completed)
+        public function __construct($title, $description, $completed)
         {
-
+            $this->title = $title;
+            $this->description = $description;
+            $this->completed = $completed;
         }
 
-        public function markAsCompleted()
+        public function markAsCompleted($completed)
         {
-            
+            $this->completed = 1;
         }
 
-        public function markAsIncomplete()
+        public function markAsIncomplete($completed)
         {
-            
+            $this->completed = 0;
         }
 
         public function getTitle()
         {
-            
+            return $this->title . PHP_EOL;
         }
 
         public function getDescription()
         {
-            
+            return $this->description . PHP_EOL;
         }
 
         public function isCompleted()
         {
-            
+            if ($this->completed == 0)
+            {
+                echo "tarefa não concluída" . PHP_EOL;
+            } elseif ($this->completed == 1)
+            {
+                echo 'tarefa concluída';
+            }
+
         }
 
     }
+
+    $tarefa1 = new Task('lavar banheiro', 'sábado de manhã', 0);
+
+    echo $tarefa1->getTitle();
+
+    echo $tarefa1->getDescription();
+
+    echo $tarefa1->isCompleted();
+
+    $tarefa1->markAsCompleted(1);
+
+    echo $tarefa1->isCompleted();
